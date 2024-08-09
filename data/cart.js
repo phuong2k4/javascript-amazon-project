2e85f958051baefe0d09736440fad8a56e4f0843
phuong2k4 <phuongsepfg@gmail.com>
2024-08-09 23:13:54 +0700

    save to localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,18 @@
-export let cart = [
-    {
-        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2
-    },
-    {
-        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 1
-    }
-];
+export let cart = JSON.parse(localStorage.getItem("cart"))
+
+if(!cart){
+    cart = [
+        {
+            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity: 2
+        }
+    ];
+}
+
+function saveCart(){
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
 
 export function addCart(productId){
     var matchingItem;
@@ -25,6 +30,7 @@ export function addCart(productId){
             quantity:1
         })
     }
+    saveCart();
   }
 
 export function removeCart(productId) {
@@ -35,4 +41,5 @@ export function removeCart(productId) {
         }
     })
     cart = newCart
+    saveCart()
 }
