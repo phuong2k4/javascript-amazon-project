2b6773fe626ad294404257a351d14824118aacec
phuong2k4 <phuongsepfg@gmail.com>
2024-08-12 21:59:32 +0700

    regenerate html when updating delivery option

--- data/cart.js
+++ data/cart.js
@@ -5,7 +5,12 @@ if(!cart){
         {
             productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             quantity: 2,
-            deliveryOptionId: '1'
+            deliveryOptionId: 1
+        },
+        {
+            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity: 2,
+            deliveryOptionId: 1
         }
     ];
 }
